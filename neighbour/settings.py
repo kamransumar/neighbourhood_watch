3456cae52eb754fafc1e38b09a0d059283822c90
kamransumar <kamransumar@gmail.com>
2018-11-26 01:22:23 +0300

    fixed some errors

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -110,15 +110,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'neighbour.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
