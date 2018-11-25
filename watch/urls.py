f396f404eb46fbe74c1d1c662b512825402c996b
kamransumar <kamransumar@gmail.com>
2018-11-25 16:36:54 +0300

    added the urls

---
+++ watch/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$',views.index,name='Index'),
+    url(r'^notifications',views.notification, name='notifications'),
+    url(r'^blog',views.blog, name='blog'),
+    url(r'^health',views.health, name='health'),
+    url(r'^authorities',views.authorities, name='authorities'),
+    url(r'^businesses',views.businesses, name='businesses'),
+    url(r'^view/blog/(\d+)',views.view_blog,name='view_blog'),
+    url(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+    url(r'^new/blogpost$',views.new_blogpost, name='new-blogpost'),
+    url(r'^new/business$',views.new_business, name='new-business'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
