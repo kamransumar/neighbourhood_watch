b93dde1793ad4dafeb514023003f7520c37681f1
kamransumar <kamransumar@gmail.com>
2018-11-25 16:37:41 +0300

    added the forms

---
+++ watch/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import notifications,Business,Profile,BlogPost
+
+
+class notificationsForm(forms.ModelForm):
+    class Meta:
+        model=notifications
+        exclude=['author','neighbourhood']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['username']
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model=BlogPost
+        exclude=['username','neighbourhood','avatar']
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        exclude=['owner','neighbourhood']
